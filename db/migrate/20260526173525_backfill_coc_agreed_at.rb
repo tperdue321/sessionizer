10ef399020ee364d747ea49db4078976fc07ffed
Travis Perdue <perduetravis@gmail.com>
2026-05-26 12:57:27 -0500

    use ActiveRecord instead of raw SQL

--- db/migrate/20260526173525_backfill_coc_agreed_at.rb
+++ db/migrate/20260526173525_backfill_coc_agreed_at.rb
@@ -1,15 +1,10 @@
 class BackfillCocAgreedAt < ActiveRecord::Migration[7.2]
   def up
-    ActiveRecord::Base.connection.execute(<<~SQL)
-      UPDATE participants
-      SET coc_agreed_at = coc.created_at
-      FROM (
-        SELECT DISTINCT ON (participant_id) participant_id, created_at
-        FROM code_of_conduct_agreements
-      ) AS coc
-      WHERE participants.id = coc.participant_id
-      AND participants.coc_agreed_at IS NULL
-    SQL
+     Participant.where(coc_agreed_at: nil).find_each do |participant|
+       aggreement = CodeOfConductAgreement.where(participant_id: participant.id)
+         .order(created_at: :desc).first
+      participant.update_column(:coc_agreed_at, aggreement.created_at) if aggreement
+    end
   end
 
   def down
